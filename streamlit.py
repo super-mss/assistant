80ac0b547ed1bb7277d8f966000ac76db0cd21f8
super-mss <135736537+super-mss@users.noreply.github.com>
2024-06-10 13:40:41 +0900

    Update streamlit.py

--- streamlit.py
+++ streamlit.py
@@ -52,6 +52,8 @@ with st.sidebar:
                 file_batch = client.beta.vector_stores.file_batches.upload_and_poll(
                     vector_store_id=VECTOR_STORE_ID, files=st.session_state.uploaded_files
                 )
+                uploaded_files = None
+                st.session_state.uploaded_files = None
                 st.subheader(f"",divider="rainbow")
                 st.info(file_batch.status)
                 if(file_batch.status=="completed") :
@@ -59,8 +61,7 @@ with st.sidebar:
                         assistant_id=ASSISTANT_ID,
                         tool_resources={"file_search": {"vector_store_ids": [VECTOR_STORE_ID]}},
                     )
-                    st.success("파일 업로드 및 벡터 저장소 업데이트 완료")
-                st.session_state.uploaded_files = None
+                    st.success("파일 업로드 및 벡터 저장소 업데이트 완료")                
             except Exception as e:
                 st.error(f"처리 중 오류 발생: {e}")
             
